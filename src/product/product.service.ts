1fdf858463d99f9a5a9266229de2c215cbd6371c
FunkyReza <reza@koco.com.sg>
2022-07-06 00:30:03 +0700

    chore: update ci/cd

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -30,7 +30,7 @@ export class ProductService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product reza jago ngoding`;
+    return `This action returns a #${id} product reza jago ngoding eh tapi boong`;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
