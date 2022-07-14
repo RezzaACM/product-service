250af3b8a3f6eb8c998e70c151c17cbd379abea7
FunkyReza <reza@koco.com.sg>
2022-07-15 00:24:36 +0700

    fix: update file env

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -30,7 +30,7 @@ export class ProductService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} jago ngoding eh tapi boong`;
+    return `This action returns a #${id} jago ngoding`;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
